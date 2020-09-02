ef40f1744a05c838809f2cea82e67104b9d3f565
日野みなみ <hinominami21@gmail.com>
2020-09-02 18:13:54 +0900

    no message

--- components/NewReports.vue
+++ components/NewReports.vue
@@ -5,16 +5,17 @@
     )
       a.reports-button(:href="item.link")
         .reports-button-head
-          .reports-button__img
+          .reports-button-head__img
             img(:src="item.thumbnail")
-          .reports-button__circle
+          .reports-button-head__circle
             p(v-html="item.creatorName")
         .reports-button-body
-          .reports-button__text
+          .reports-button-body__text
             p(v-html="item.pubDate")
-            h4.reports-button__title(v-html="item.title")
-        a.button.button-right.isSp(href="#")
-          span 続きを読む
+            h4.reports-button-body__title(v-html="item.title")
+            .hoge
+              a.button.button-right.isSp(href="#")
+                span 続きを読む
         //冒頭文
           p.reports__item__content(v-html="getContent(item.content)")
         //日付
@@ -61,27 +62,42 @@
 
 <style lang="scss">
   .reports {
-    @include mediaquery-not-sm {
-      .reports-button {
+    .reports-button {
+      margin-top: 3.5rem;
+      margin-bottom: 3.5rem;
+      @include mediaquery-not-sm {
         display: flex;
-        margin-top: 3.5rem;
-        margin-bottom: 3.5rem;
-
-        &-head {
+      }
+      &-head {
+        margin-top: 8rem;
+        z-index: 2;
+        @include mediaquery-not-sm {
           flex-basis: 45%;
-          z-index: 2;
+        }
+        @include mediaquery-sm {
+          margin-bottom: -5rem;
+          text-align: center;
+        }
 
-          .reports-button__img {
+        &__img {
+          @include mediaquery-not-sm {
             text-align: right;
-
-            img {
-              width: 100%;
-              height: 306px;
-              object-fit: cover;
-            }
           }
 
-          .reports-button__circle {
+          img {
+            width: 100%;
+            object-fit: cover;
+            @include mediaquery-not-sm {
+              height: 306px;
+            }
+            @include mediaquery-sm {
+              width: 120px;
+            }
+          }
+        }
+
+        &__circle {
+          @include mediaquery-not-sm {
             position: relative;
             z-index: 1;
             margin-right: auto;
@@ -106,61 +122,75 @@
               transform: skew(-20deg)
             }
           }
-
-        }
-
-        &-body {
-          z-index: 1;
-          flex-basis: 55%;
-          margin-top: -2rem;
-
-          .reports-button__text {
-            top: 0;
-            width: 100%;
-            padding-top: 3.5rem;
-            padding-bottom: 3.5rem;
-            padding-left: 5rem;
-            position: relative;
-            z-index: 1;
-            color: #414141;
-            display: inline-block;
-
-            &:before {
-              content: "";
-              display: block;
-              position: absolute;
-              height: 100%;
-              width: 100%;
-              background: #ffffff;
-              z-index: -2;
-              top: 0;
-              left: 0;
-              transform-origin: top left;
-              transform: skew(-20deg);
-            }
-
-            .text {
-              margin-bottom: 2.5rem;
+          @include mediaquery-sm {
+            p {
+              color: #000000;
             }
           }
         }
+      }
 
-        transition: all 2s ease-in-out 0s;
+      &-body {
+        z-index: 1;
+        @include mediaquery-not-sm {
+          flex-basis: 55%;
+          margin-top: 5rem;
+        }
+        &__text {
+          top: 0;
+          width: 100%;
+          position: relative;
+          z-index: 0;
+          color: #414141;
+          display: inline-block;
+          @include mediaquery-not-sm {
+            padding-top: 3.5rem;
+            padding-bottom: 3.5rem;
+            padding-left: 5rem;
+          }
+          @include mediaquery-sm{
+            padding-top: 12rem;
+          }
+          &:before {
+            content: "";
+            display: block;
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            background: #ffffff;
+            z-index: -2;
+            top: 0;
+            left: 0;
+            transform-origin: top left;
+            @include mediaquery-not-sm {
+              transform: skew(-20deg);
+            }
+            @include mediaquery-sm {
+              transform: skewY(15deg);
+            }
+          }
+          .text {
+            margin-bottom: 2.5rem;
+          }
+        }
+      }
+
+      transition: all 2s ease-in-out 0s;
 
 
-        &:hover {
+      &:hover {
+        @include mediaquery-not-sm {
           transform: translateX(-0.3em);
           filter: opacity(0.76);
           transition: all 0.4s ease-in-out 0s;
 
 
-          .reports-button__img,
-          .reports-button__circle:before,
-          .reports-button__text:before {
+          .reports-button-head__img,
+          .reports-button-head__circle:before,
+          .reports-button-body__text:before {
             box-shadow: 0 6px 6px 0 rgba(0, 0, 0, 1);
           }
         }
-
       }
 
       .button-right {
@@ -175,48 +205,49 @@
         margin-top: 2.5rem;
         margin-bottom: 0;
       }
-    }
-
-    @include mediaquery-sm {
-
-      .button {
-        position: relative;
-        z-index: 1;
+      .hoge {
         text-align: center;
-        margin-left: auto;
-        margin-right: auto;
-        color: #ffffff;
-        display: inline-block;
-        padding-top: 1rem;
-        padding-bottom: 1rem;
-        padding-left: 1.5rem;
-        padding-right: 1.5rem;
 
-        &:before {
-          content: "";
-          display: block;
-          position: absolute;
-          height: 100%;
-          width: 100%;
-          z-index: -2;
-          background: #867C84;
-          top: 0;
-          left: 0;
-        }
-
-
-        .button-right {
-          margin: 0;
+        .button {
+          position: relative;
+          z-index: 1;
+          text-align: center;
+          margin-top: 0.75rem;
+          margin-bottom: 0.75rem;
+          margin-left: auto;
+          margin-right: auto;
+          color: #ffffff;
+          display: inline-block;
+          padding-top: 1rem;
+          padding-bottom: 1rem;
+          padding-left: 1.5rem;
+          padding-right: 1.5rem;
 
           &:before {
-            transform: skew(-20deg)
+            content: "";
+            display: block;
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            z-index: -2;
+            background: #867C84;
+            top: 0;
+            left: 0;
           }
 
-          &:hover {
-            color: #ffffff;
+
+          .button-right {
+            margin: 0;
 
             &:before {
-              background: #414141;
+              transform: skew(-20deg)
+            }
+
+            &:hover {
+              color: #ffffff;
+              &:before {
+                background: #414141;
+              }
             }
           }
         }
